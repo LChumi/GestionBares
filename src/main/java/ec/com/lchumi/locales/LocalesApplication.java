b4d267724c01dfff20004e3fdf7d6e5e98863b79
LChumi <112025391+LChumi@users.noreply.github.com>
2024-08-18 17:08:04 -0500

    Fix(fixing dialect to databse and adding info to swagger)

--- src/main/java/ec/com/lchumi/locales/LocalesApplication.java
+++ src/main/java/ec/com/lchumi/locales/LocalesApplication.java
@@ -1,9 +1,12 @@
 package ec.com.lchumi.locales;
 
+import ec.com.lchumi.locales.config.OpenAPIConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(OpenAPIConfiguration.class)
 public class LocalesApplication {
 
 	public static void main(String[] args) {
